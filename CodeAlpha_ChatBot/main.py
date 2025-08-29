567027d4324065cd3e79159ee86d69833b3a9bc6
Ushna Khan <ushnakhan831@gmail.com>
2025-08-29 13:51:32 +0500

    Update main.py

--- CodeAlpha_ChatBot/main.py
+++ CodeAlpha_ChatBot/main.py
@@ -1,1 +1,17 @@
+def chatbot():
+    print("Chatbot: Hello! I'm your simple chatbot. Type 'bye' to exit.")
+    
+    while True:
+        user_input = input("You: ").lower()
+        
+        if user_input in ["hello", "hi", "hey"]:
+            print("Chatbot: Hi there! 👋")
+        elif user_input in ["how are you", "how are you doing"]:
+            print("Chatbot: I'm fine, thanks! How about you?")
+        elif user_input == "bye":
+            print("Chatbot: Goodbye! Have a great day! 👋")
+            break
+        else:
+            print("Chatbot: Sorry, I don't understand that.")
+chatbot()
 //
